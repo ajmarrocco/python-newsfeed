db7eae394225459f197d32bcf3daf59a324c80cd
ajmarrocco <ajmarrocco@gmail.com>
2022-07-02 12:40:50 -0400

    add comments to app/db/__init__.py

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -4,9 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
+# Called because we need use a .env file
 load_dotenv()
 
 # connect to database using env variable
+# engine manages connection of database
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+# Session creates temporary connections to perform CRUD methods
 Session = sessionmaker(bind=engine)
+# Base aps models to real MySQL tables
 Base = declarative_base()
