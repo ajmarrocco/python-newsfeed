268b927e622ccbe7d0faa0d886c3857d2a455b96
ajmarrocco <ajmarrocco@gmail.com>
2022-07-02 22:10:02 -0400

    create comment model

--- app/models/Comment.py
+++ app/models/Comment.py
@@ -4,17 +4,14 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
 # inherits from Base class created in app/db
-class Post(Base):
-    __tablename__ = 'posts'
+class Comment(Base):
+    __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
-    title = Column(String(100), nullable=False)
-    post_url = Column(String(100), nullable=False)
-    # referecnes user table
+    comment_text = Column(String(255), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
-    # Query for post should also return info about user
+    # Query for comments should also return info about user
     user = relationship('User')
-    # Cascade delete, deletes all comments associated with posts
-    comments = relationship('Comment', cascade='all,delete')
