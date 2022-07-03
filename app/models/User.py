4923dec9ce5b7335d5c18e18dacbb66a89f73516
ajmarrocco <ajmarrocco@gmail.com>
2022-07-02 21:43:24 -0400

    add validate email and password functions

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,6 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
 
 # inherits from Base class created in app/db
 class User(Base):
@@ -8,3 +9,18 @@ class User(Base):
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
+
+    @validates('email')
+    def validate_email(self, key, email):
+        # make sure email address contains @ character
+        # throws false if not happening
+        assert '@' in email
+        # returns value of email column
+        return email
+
+    @validates('password')
+    def validate_password(self, key, password):
+        # checks length of password
+        assert len(password) > 4
+
+        return password
