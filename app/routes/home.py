f45fa76b708ee6b26334541f3846603fa3920ad0
ajmarrocco <ajmarrocco@gmail.com>
2022-07-03 21:38:03 -0400

    add session variables to login method

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,5 +1,5 @@
 # Imports functions Blueprint and render_template from flask
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 from app.models import Post
 from app.db import get_db
 
@@ -13,11 +13,19 @@ def index():
     db = get_db()
     posts = db.query(Post).order_by(Post.created_at.desc()).all()
 
-    return render_template('homepage.html', posts=posts)
+    return render_template(
+        'homepage.html', 
+        posts=posts, 
+        loggedIn = session.get('loggedIn')
+    )
 
 @bp.route('/login')
 def login():
-    return render_template('login.html')
+    # not login in
+    if session.get('loggedIn') is None:
+        return render_template('login.html')
+        
+    return redirect('/dashboard')
 
 # <id> represents a parameter which is why we include a single parameter function
 @bp.route('/post/<id>')
@@ -29,5 +37,6 @@ def single(id):
     # render single post template
     return render_template(
         'single-post.html',
-        post=post
+        post=post,
+        loggedIn = session.get('loggedIn')
     )
