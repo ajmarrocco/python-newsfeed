d363cdb5d921c19b5312c4b9b9126f50a7e2dd72
ajmarrocco <ajmarrocco@gmail.com>
2022-07-03 21:10:15 -0400

    set up error message when signup fails

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,4 @@
-# from crypt import methods
-# from traceback import print_tb
-import email
+import sys
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
@@ -13,16 +11,24 @@ def signup():
     # returns data as Python Dictionary
     data = request.get_json()
     db = get_db()
-    # create a new user
-    newUser = User(
-        # bracket notation for Python dictionary
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
-    # save in database
-    db.add(newUser)
-    db.commit()
+    try:
+        # create a new user
+        newUser = User(
+            # bracket notation for Python dictionary
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
+        # save in database
+        db.add(newUser)
+        db.commit()
+    except:
+        # insert failed, so send error message
+        print(sys.exe_info()[0])
+
+        # insert failed, so rollback and send error to front end
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
     
     # returns new User wtih ID number
     return jsonify(id = newUser.id)
